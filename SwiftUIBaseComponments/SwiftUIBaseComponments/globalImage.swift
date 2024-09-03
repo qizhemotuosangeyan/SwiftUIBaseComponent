525a1f2e5f6d670fbaf84e2da48cafee658aef77
qizhemotuosangeyan <44314214+qizhemotuosangeyan@users.noreply.github.com>
2024-09-03 18:40:53 +0800

    更新到最新仓库现状

---
+++ SwiftUIBaseComponments/SwiftUIBaseComponments/globalImage.swift
@@ -0,0 +1,21 @@
+//
+//  globalImage.swift
+//  SwiftUIBaseComponments
+//
+//  Created by 千千 on 2024/8/10.
+//
+
+import SwiftUI
+
+struct globalImage: View {
+    var body: some View {
+        Image(systemName: "globe")
+    }
+}
+
+
+
+
+#Preview {
+    globalImage()
+}
